e2959bf529d2dc180fe3d2082fe5ad17b3c82b27
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-18 13:52:35 -0400

    Program working now more or less.

--- word_histogram.py
+++ word_histogram.py
@@ -1,4 +1,22 @@
-def histogram():
-    book = open(sample.txt)
+import string
 
-    book.close()
+def histogram():
+    with open("sample.txt") as opened_file:
+        book = opened_file.read()
+
+    for x in string.punctuation:
+        book = book.replace(x,'')
+
+    book_histogram = {}
+
+    for word in book.split():
+        if word in book_histogram.keys():
+            book_histogram[word] +=1
+        else:
+            book_histogram[word] = 1
+
+    for key, value in book_histogram.items():
+        if key >= 2:
+            print(key, value)
+
+histogram()
